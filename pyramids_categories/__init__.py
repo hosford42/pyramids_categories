7615f857c76934b17e5b8e74c38273ae383b6da3
Aaron Hosford <hosford42@gmail.com>
2015-09-03 23:21:37 -0500

    Initial Commit

---
+++ pyramids_categories/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyramids Categories
+===================
+Pre-compiled category files for the Pyramids Parser.
+"""
+
+__author__ = 'Aaron Hosford'
+__version__ = '1.0.0'
+__all__ = []
+
+# TODO: Someday, add code to automatically generate the .ctg files from
+#       Kevin Atkinson's wordlist.
