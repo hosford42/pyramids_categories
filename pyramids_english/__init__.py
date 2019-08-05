cab7a4aaa4fd35fbccdf5ea30edb3b2468a71966
Aaron Hosford <hosford42@gmail.com>
2019-08-04 23:59:13 -0500

    Moved English-specific tokenizer to pyramids_english

--- pyramids_english/__init__.py
+++ pyramids_english/__init__.py
@@ -11,6 +11,7 @@ import os
 from pyramids.loader import ModelLoader
 from pyramids.parsing import Parser
 from pyramids.repl import repl
+from pyramids_english.tokenization import EnglishTokenizer
 
 __author__ = 'Aaron Hosford'
 __version__ = '2.0.0'
@@ -26,13 +27,16 @@ __all__ = [
 #       Kevin Atkinson's wordlist.
 
 
-def get_model_loader(verbose=False):
-    return ModelLoader('english', os.path.join(os.path.dirname(__file__), 'data'), verbose=verbose)
+def get_model_loader(path=None, verbose=False):
+    path = path or os.path.join(os.path.dirname(__file__), 'data')
+    return ModelLoader('english', path, verbose=verbose)
 
 
-def load_model(verbose=False):
-    model_loader = get_model_loader(verbose)
-    return model_loader.load_model()
+def load_model(path=None, verbose=False):
+    model_loader = get_model_loader(path, verbose)
+    discard_spaces = model_loader.model_config_info.discard_spaces
+    tokenizer = EnglishTokenizer(discard_spaces)
+    return model_loader.load_model(tokenizer)
 
 
 def get_parser(model=None):
@@ -41,6 +45,12 @@ def get_parser(model=None):
     return Parser(model)
 
 
+def tokenize(text: str, discard_spaces: bool = True):
+    return EnglishTokenizer(discard_spaces).tokenize(text)
+
+
 def main():
     model_loader = get_model_loader()
-    repl(model_loader)
+    discard_spaces = model_loader.model_config_info.discard_spaces
+    tokenizer = EnglishTokenizer(discard_spaces)
+    repl(model_loader, tokenizer)
---
+++ pyramids_english/tokenization.py
@@ -0,0 +1,67 @@
+from pyramids.tokenization import Tokenizer
+
+__all__ = [
+    'EnglishTokenizer',
+]
+
+
+class EnglishTokenizer(Tokenizer):
+
+    def __init__(self, discard_spaces=True):
+        self._discard_spaces = bool(discard_spaces)
+        self.contractions = ("'", "'m", "'re", "'s", "'ve", "'d", "'ll")
+
+    @property
+    def discard_spaces(self):
+        return self._discard_spaces
+
+    @staticmethod
+    def is_word_char(char):
+        return char.isalnum() or char == "'"
+
+    def tokenize(self, text):
+        last_char = ''
+        start = 0
+        end = 0
+        non_space = False
+
+        for index, char in enumerate(text):
+            if start != end and last_char != char and not (self.is_word_char(last_char) and self.is_word_char(char)):
+                if not self.discard_spaces or non_space:
+                    token = text[start:end]
+                    if token.endswith(self.contractions):
+                        split = token.split("'")
+                        if len(split) > 1 and (len(split) != 2 or split[0]):
+                            yield "'".join(split[:-1]), start, end - len(split[-1])
+                        yield "'" + split[-1], end - len(split[-1]), end
+                    elif token[-2:].lower() in ('am', 'pm') and token[:-2].isdigit():
+                        yield token[:-2], start, end - 2
+                        yield token[-2:], end - 2, end
+                    elif token[-1:].lower() in ('a', 'p') and token[:-1].isdigit():
+                        yield token[:-1], start, end - 1
+                        yield token[-1:], end - 1, end
+                    else:
+                        yield token, start, end
+                    del token
+                start = end
+                non_space = False
+            end += 1
+            last_char = char
+            if not char.isspace():
+                non_space = True
+
+        if start < end and (not self.discard_spaces or non_space):
+            token = text[start:end]
+            if token.endswith(self.contractions):
+                split = token.split("'")
+                if len(split) > 1:
+                    yield "'".join(split[:-1]), start, end - len(split[-1])
+                yield "'" + split[-1], end - len(split[-1]), end
+            elif token[-2:].lower() in ('am', 'pm') and token[:-2].isdigit():
+                yield token[:-2], start, end - 2
+                yield token[-2:], end - 2, end
+            elif token[-1:].lower() in ('a', 'p') and token[:-1].isdigit():
+                yield token[:-1], start, end - 1
+                yield token[-1:], end - 1, end
+            else:
+                yield token, start, end
